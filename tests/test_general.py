899fae22c2a08cf183c8fdd7dbe071455533cbaa
Patrickdailey <patrickdailey86@gmail.com>
2021-12-01 07:50:40 +0100

    added tests

---
+++ tests/test_general.py
@@ -0,0 +1,9 @@
+import pytest
+
+# from app.xx import testFunction
+# @pytest.mark.parametrize("var1, var2, result", [
+#     (1, 2, 3),
+#     (2, 2, 5),
+# ])
+# def test_practice(var1, var2, result):
+#     assert testFunction(var1, var2) == result
